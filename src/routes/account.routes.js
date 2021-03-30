d24b77fe5f0435b0534b7721b055dda1b8ee2ade
Lucas Castro <lucascastro@MacBook-Air-de-Lucas.local>
2021-03-30 10:22:04 -0300

    continuando aplicação

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -2,6 +2,17 @@ const express = require('express');
 const verifyAccount = require('../middlewares/verifyAccount');
 const accountRoute = express.Router();
 
+function getBalance(statement){
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type == 'credit'){
+      return acc+ operation.amount;
+    }else{
+      return acc - operation.amount;
+    }
+  }, 0);
+  return balance;
+}
+
 accountRoute.use(verifyAccount);
 
 accountRoute.get('/statement', (request, response) => {
@@ -29,6 +40,21 @@ accountRoute.post('/withdraw', (request, response) => {
   const { amount } = request.body;
 
   const {customer} = request;
+
+  const balance = getBalance(customer.statement);
+  if(balance < amount){
+    return response.status(400).json({error: "Insufficient funds!"});
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit',
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 })
 
 module.exports = accountRoute;
